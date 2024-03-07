5a6374390eac018eb48ae426ea0c257dd65dc36c
Nayeon Chae <155615768+NY-Chae@users.noreply.github.com>
2024-03-07 09:12:34 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 4.42 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int solution(int num1, int num2) 
+
+{
+    int answer = 0;
+    if(!(num1>0 && num1<=100) || !(num2>0 && num2<=100)) 
+    {
+        printf("잘못된 매개변수입니다.");
+        return -1;
+    }
+    answer = num1/num2;
+    return answer;
+}
