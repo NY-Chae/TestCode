474d9d09c66a5ea1634fc65554e5686557fce5c7
Nayeon Chae <155615768+NY-Chae@users.noreply.github.com>
2024-03-20 10:11:33 +0900

    [level 1] Title: 대충 만든 자판, Time: 10.74 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func solution(_ keymap: [String], _ targets: [String]) -> [Int] {
+    var dict = [String: Int]()
+    var keymap = keymap.map { $0.map { String($0) } }
+    var result = [Int]()
+    
+    for k in keymap {
+        for i in k {
+            if dict[i] == nil {
+                dict[i] = k.firstIndex(of: i)!
+            } else {
+                dict[i] = min(dict[i]!, k.firstIndex(of: i)!)
+            }
+        }
+    }
+    
+    for target in targets {
+        var sum = 0
+        for t in target.map { String($0) } {
+            if dict[t] == nil {
+                sum = -1
+                break
+            } else {
+                sum += dict[t]! + 1
+            }
+        }
+        result.append(sum)
+    }
+    
+    return result
+}
