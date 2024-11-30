6918867da627ac93272934a34f3969a55e1c1cf1
Nayeon Chae <155615768+NY-Chae@users.noreply.github.com>
2024-11-30 19:55:39 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 59.56 ms, Memory: 12.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let s1 = readLine()!
+
+var result = ""
+//이러면 s1의 길이만큼 루프
+for i in s1 {
+    
+    if (i.isLowercase){
+        result += i.uppercased()
+    }else if(i.isUppercase){
+        result += i.lowercased()
+    }
+}
+
+print(result)
